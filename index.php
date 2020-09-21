fcd93f65a59ec65d60aafc110427da9ee41e8313
Jonny <jonny788@live.co.uk>
2020-09-21 13:51:28 +0100

    added delete button to index to remove from sql

--- index.php
+++ index.php
@@ -2,8 +2,26 @@
     //this contains the connection code so it can be imported as needed
     include('config/db_config.php');
 
+    
+    if(isset($_POST['delete'])){
+        //get value of the form on the page 
+        $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+
+        //query telling it to delete the row associated with the id submitted
+        $sql ="DELETE FROM devices WHERE id = $id_to_delete";
+
+        if(mysqli_query($conn,$sql)){
+            //success
+            header('Location: index.php');
+        }
+        else{
+            echo 'query error: ' . mysqli_error($conn);
+        }
+    }
+    
+    
     //select all devices
-    $sql = 'SELECT name, ip, status FROM devices';
+    $sql = 'SELECT id, name, ip, status FROM devices';
 
     //issue query
     $result = mysqli_query($conn,$sql);
@@ -20,12 +38,12 @@
     //print_r($devices);
 
     
-
+        //store id of device somewhere or search and delete row by the name (31)
 ?>
 <html>
 <body>
 
-
+    
     <?php include('templates/header.php'); ?>
 
     <h4 class = "center grey-text">devices</h4>
@@ -36,10 +54,17 @@
                 <div class="col s6 md3 ">
                     <div class=" card z-depth-0">
                         <div class="card-content center">
+                            <i class="large material-icons">devices_other</i>
                             <h6> <?php echo htmlspecialchars($device['name']); ?> </h6>
                             <h6> <?php echo htmlspecialchars($device['ip']); ?> </h6>
                             <h6> <?php echo htmlspecialchars($device['status']); ?> </h6>
-                        </div>    
+                        </div>
+                        <!-- delete from -->
+                        <form action="index.php" method="POST">
+                            <input type="hidden" name="id_to_delete" value ="<?php echo $device['id'] ?>">
+                            <input type="submit" name="delete" value=Delete class="btn brand z-depth-0">
+                        </form>
+                           
                     </div>
                 </div>
 
--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,7 @@
     <title>IoT Systems</title>
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <style type="text/css">
         .brand{
             background: #cbb097 !important;
